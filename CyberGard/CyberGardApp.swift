6ed31b0e327ef5d52851658a31f1bc64811d1abe
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-06-18 02:50:06 +0200

    Initial Commit

---
+++ CyberGard/CyberGardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyberGardApp.swift
+//  CyberGard
+//
+//  Created by Zane Neurothrone on 2025-06-18.
+//
+
+import SwiftUI
+
+@main
+struct CyberGardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
