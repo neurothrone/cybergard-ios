7faaa95eebbcd6259dfe9e45aace930566586a82
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-06-20 14:59:21 +0200

    add DateFormatter extension for API date formatting

---
+++ CyberGard/Extensions/DateExtensions.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension DateFormatter {
+  static let apiFormat: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    return formatter
+  }()
+}
