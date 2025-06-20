4d21b618736ba3d4fdba304619d0c81d6207dee1
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-06-20 15:00:32 +0200

    replace ContentView with EmailScreen to display email reports

--- CyberGard/ContentView.swift
+++ CyberGard/ContentView.swift
@@ -1,24 +1,11 @@
-//
-//  ContentView.swift
-//  CyberGard
-//
-//  Created by Zane Neurothrone on 2025-06-18.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  var body: some View {
+    EmailScreen(reports: [])
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
