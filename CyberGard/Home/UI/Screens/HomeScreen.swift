d37da1393049c4017bad90eb5b23ee68db52de69
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-06-23 07:16:18 +0200

    add placeholder content

--- CyberGard/Home/UI/Screens/HomeScreen.swift
+++ CyberGard/Home/UI/Screens/HomeScreen.swift
@@ -2,7 +2,26 @@ import SwiftUI
 
 struct HomeScreen: View {
   var body: some View {
-    Text("Home")
+    NavigationStack {
+      VStack(alignment: .center, spacing: 8) {
+        Group {
+          Text("CyberGard")
+            .font(.largeTitle)
+          Text(
+            "Your trusted source for safety of phone numbers, emails, URLs, and more."
+          )
+          .font(.title3)
+          Text(
+            "This app is currently under construction. Stay tuned for updates!"
+          )
+          .font(.callout)
+        }
+        .multilineTextAlignment(.center)
+      }
+      .navigationTitle("Home")
+      .navigationBarTitleDisplayMode(.inline)
+      .frame(maxWidth: UIScreen.main.bounds.width * 0.9)
+    }
   }
 }
 
