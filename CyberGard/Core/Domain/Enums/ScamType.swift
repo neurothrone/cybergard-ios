225c470286cb4bb045cd3e0b2aa998e0faff8638
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-06-27 01:46:11 +0200

    add: update ScamType enum with new cases and titles

--- CyberGard/Core/Domain/Enums/ScamType.swift
+++ CyberGard/Core/Domain/Enums/ScamType.swift
@@ -1,30 +1,86 @@
 import Foundation
 
 enum ScamType: Identifiable, CaseIterable {
-  case spamOrTelemarketing
+  case extortionOrBlackmail
+  case fakeSupport
+  case harrasmentOrThreats
+  case impersonation
+  case maliciousContent
+  case phishing
   case scamOrFraud
-  case wrongNumber
-  case harassment
+  case spamOrTelemarketing
+  case suspiciousLinkOrDomain
   case other
 
   var id: Self { self }
 
   static var `default`: Self {
-    return .spamOrTelemarketing
+    return .fakeSupport
+  }
+}
+
+extension ScamType {
+  var title: String {
+    return switch self {
+    case .extortionOrBlackmail:
+      "Extortion / Blackmail"
+    case .fakeSupport:
+      "Fake Support"
+    case .harrasmentOrThreats:
+      "Harassment / Threats"
+    case .impersonation:
+      "Impersonation"
+    case .maliciousContent:
+      "Malicious Content"
+    case .phishing:
+      "Phishing"
+    case .scamOrFraud:
+      "Scam / Fraud"
+    case .spamOrTelemarketing:
+      "Spam / Telemarketing"
+    case .suspiciousLinkOrDomain:
+      "Suspicious Link / Domain"
+    case .other:
+      "Other (please specify)"
+    }
+  }
+}
+
+extension ScamType {
+  static var emailOnlyCases: [ScamType] {
+    return [
+      .fakeSupport,
+      .extortionOrBlackmail,
+      .harrasmentOrThreats,
+      .impersonation,
+      .maliciousContent,
+      .phishing,
+      .scamOrFraud,
+      .spamOrTelemarketing,
+      .other,
+    ]
   }
 
-  var title: String {
-    switch self {
-    case .spamOrTelemarketing:
-      return "Spam / Telemarketing"
-    case .scamOrFraud:
-      return "Scam / Fraud"
-    case .wrongNumber:
-      return "Wrong Number"
-    case .harassment:
-      return "Harassment"
-    case .other:
-      return "Other (please specify)"
-    }
+  static var phoneOnlyCases: [ScamType] {
+    return [
+      .fakeSupport,
+      .harrasmentOrThreats,
+      .impersonation,
+      .scamOrFraud,
+      .spamOrTelemarketing,
+      .other,
+    ]
+  }
+
+  static var urlOnlyCases: [ScamType] {
+    return [
+      .fakeSupport,
+      .impersonation,
+      .maliciousContent,
+      .phishing,
+      .scamOrFraud,
+      .suspiciousLinkOrDomain,
+      .other,
+    ]
   }
 }
